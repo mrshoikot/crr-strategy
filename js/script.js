d34bd1600915bc1ca61ab1948cc384a64c0c6e95
mrshoikot <mrshoikot@gmail.com>
2022-11-15 21:49:43 +0600

    intialized v0.1

--- js/script.js
+++ js/script.js
@@ -15,60 +15,54 @@
 /* Custom Js (Jquery) */
 
 
-jQuery(function($) { 'use strict';
+jQuery(function ($) {
+    'use strict';
 
 
-    $(window).on('load', function() {
+    $(window).on('load', function () {
 
 
 
     });
 
 
-    $(document).ready(function() {
+    $(document).ready(function () {
+
+        $('#preloader-switch').click(function(){
+            $('.flyer-wrap').addClass('active');
+            $('#preloader').addClass('hide');
+        })
+
+        var movementStrength = 23;
+        var height = movementStrength / $(document).height();
+        var width = movementStrength / $(document).width();
+        $("body").on("mousemove", function (e) {
+            var pageX = e.pageX - $(document).width() / 2,
+                pageY = e.pageY - $(document).height() / 2,
+                newvalueX = width * pageX * -1,
+                newvalueY = height * pageY * -1,
+                elements = $(".lm-animated-bg");
+            elements.addClass("transition");
+            elements.css({
+                "background-position":
+                    "calc( 0% + " + newvalueX + "px ) calc( 0% + " + newvalueY + "px )",
+            });
+            setTimeout(function () {
+                elements.removeClass("transition");
+            }, 300);
+        });
 
         var toggleState = false;
 
-        $('.flip').click(function() {
+        $('.flip').click(function () {
             $('.flyer').toggleClass('flip');
+            console.log('fire');
             $('.front, .back').toggleClass('folded');
         });
-       
-        var $folded = $('.folded').oriDomi({
-            ripple: 1,
-            shading: 'soft',
-            shadingIntensity: 0,
-            perspective: 1000
-        });
-        $folded.oriDomi('accordion', 50);
-       
-        $('.fold').click(function() {
-            if (toggleState) {
-                $(this).html("Fold");
-                $folded.oriDomi('accordion', 0);
-                $('.container').animate({
-                    marginLeft: -200
-                }, 900, function() {});
-            } else {
-                $(this).html("Unfold");
-                $folded.oriDomi('accordion', 50);
-                $('.container').animate({
-                    marginLeft: 0
-                }, 900, function() {});
-            }
-            toggleState = !toggleState;
-        });
-       
-       
-        $('span#hotspot-one').hover(function() {
-            $('.tooltip.tip-one').fadeToggle();
-        });
-       
-        $('span#hotspot-two').hover(function() {
-            $('.tooltip.tip-two').fadeToggle();
-        });
-       
-        
+
+
+
+
     });
 
 });
